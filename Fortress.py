ae64092dfacdbf9ba58cd06bf38a78842930152f
Jerry-code-oss <2557399593@qq.com>
2023-12-23 12:40:06 +0800

    样板程序

---
+++ Fortress.py
@@ -0,0 +1,27 @@
+##################################################
+##     M1---Whether is enemy recongnized---Luck ##
+##     M2---Hardware processing---Andy          ##
+##                                              ##
+##                                              ##
+##################################################
+#from multiprocessing import Process, Pipe
+#import multiprocessing
+import time
+
+class M1:
+    def __init__(self) -> None:
+        pass
+    def Model_Preparation(self):
+        pass
+    def Modle_Construct(self):
+        pass
+
+class M2:
+    def __init__(self) -> None:
+        pass
+
+
+
+
+if __name__ == "__main__": #从这里开始
+    print("1")
