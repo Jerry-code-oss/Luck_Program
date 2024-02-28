e3ee4d0766146b487e8cec230fb8811a80906cee
Lancer <59310667+Jerry-code-oss@users.noreply.github.com>
2024-02-28 10:36:29 +0800

    Create Face_Tracker.py

---
+++ Fortress/Face_Tracker.py
@@ -0,0 +1,37 @@
+import cv2
+import torch
+import numpy as np
+print(cv2.__version__)
+camera_index = 0  # 视频源索引号
+
+class Yolo_Bracket:
+    def __init__(self) -> None:
+        pass
+    def Load_Model():
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
+
+    def Capture_Video(model):
+        cap = cv2.VideoCapture(camera_index)
+        while True:
+            # 读取摄像头的一帧
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # 将图像转换为YOLO模型需要的格式
+            results = model(frame)
+            # 解析模型返回的结果
+            for i in range(len(results.pred[0])):
+                if results.names[int(results.pred[0][i][5])] == 'person':
+                    xmin, ymin, xmax, ymax = results.pred[0][i][:4].int()
+                    cv2.rectangle(frame, (int(xmin), int(ymin)), (int(xmax), int(ymax)), (255, 0, 0), 2)
+            # 显示处理后的图像
+            cv2.imshow('Capture_Human', frame)
+            # 如果按下'q'键，退出循环
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+        
+if __name__ == "__main__":
+    Yolo_Bracket.Capture_Video(Yolo_Bracket.Load_Model())
